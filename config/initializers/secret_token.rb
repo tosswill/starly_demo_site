05bcfe6578bab91ab906fddee0b99deb06e84dd3
Chris Tosswill <christosswill@gmail.com>
2012-04-06 20:28:04 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoStarly::Application.config.secret_token = 'e84cd2bea870b2c3fb06d5a8bdd132d6daf8cd47fdd21fce7d2d9ab1f7801203101186633c7a9edcc6c53aecaf54de28b6c873f75773d0b6f259bd9a9197c05c'
